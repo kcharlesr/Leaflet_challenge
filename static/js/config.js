2710fd91dd8c3e6802b96218f0c2b04ab1faa155
Kevin Ricker <kevincharlesricker@gmail.com>
2021-04-19 12:47:11 -0400

    Final

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,8 @@
+// kcharlesr
+
+
 // API key
-const API_KEY = "YOUR API KEY HERE!";
+const API_KEY = "pk.eyJ1Ijoia2NoYXJsZXNyIiwiYSI6ImNrbGszMTEzOTBua2Uyb2x5eGF6bzc4MzgifQ.RPyZ9r7TRCWFRm28kEoiVw";
+
+
+// pk.eyJ1Ijoic25hc2VlciIsImEiOiJjazE1bXUzZXIwa2pqM2RtcnRya2FodWRzIn0.PORmcOp_kg6ZnQMgMe_6jg
